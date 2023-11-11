fa51d355aa26733793420a1d0bc839e3ee3cbf2c
sufyannasir500 <sufyannasir500@gmail.com>
2023-11-11 21:02:32 +0500

    Displaying platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/image-url";
+import PlatformsIconList from "./PlatformsIconList";
 
 interface Props {
   game: Game;
@@ -12,6 +13,9 @@ const GameCard = ({ game }: Props) => {
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformsIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,10 +1,16 @@
 import useData from "./useData";
 
+export interface Platform {
+    id: number;
+    name: string;
+    slug: string;
+}
 
 export interface Game {
     id: number;
     name: string;
     background_image: string;
+    parent_platforms: {platform: Platform}[];
   }
 
 const useGames = () => useData<Game>('/games')
